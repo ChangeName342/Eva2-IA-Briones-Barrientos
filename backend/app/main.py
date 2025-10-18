86f0df38127adc7655bbcfb68dcc79f671a4755a
Ignacio Barrientos <132976388+ChangeName342@users.noreply.github.com>
2025-10-17 21:58:34 -0300

    Ajustes pandas

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 # Permitir solicitudes desde el frontend
 origins = [
-    "http://localhost:5173",  # o el puerto donde corre tu frontend
+    "https://eva2-frontend.onrender.com",  # o el puerto donde corre tu frontend
 ]
 
 app.add_middleware(
